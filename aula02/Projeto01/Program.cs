bacfcfc5d73488a4c8e8328a1e17a1c61d754942
EmersonPaduan <emerson.paduan@gmail.com>
2024-08-19 10:42:39 -0300

    aula 02 - parte 1

---
+++ aula02/Projeto01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Meu primeiro programa C#
+
+// WriteLIne escreva uma mensagem no terminal
+Console.Write("Olá");
+Console.WriteLine(" mundo!");
+
+// O sinal de + faz uma soma
+Console.WriteLine(12 + 4);
+Console.WriteLine("12 + 4");
+
+// Neste caso o sinal de + é uma concatenação (junção de texto)
+Console.WriteLine("Resposta = " + (12 + 4));
+
+Console.WriteLine("12" + "+" + 4 + "=" + 16);
+Console.WriteLine("{0} + {1} = {2}", 12, 4, 12 + 4);
