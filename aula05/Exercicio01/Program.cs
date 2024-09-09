6702f00f410f9094bdb831bd259787cdef1151d1
EmersonPaduan <emerson.paduan@gmail.com>
2024-09-09 10:04:57 -0300

    aula 05 - parte 1

---
+++ aula05/Exercicio01/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string senha;
+string resultado;
+
+// Entrada de dados
+Console.WriteLine("Digite a sua senha:");
+senha = Console.ReadLine();
+
+// processamento
+if(senha == "R10p5") // se a variável 'senha' é igual à "R10p5"
+{
+    resultado = "Acesso permitido";
+}
+else 
+{
+    resultado = "Acesso negado";
+}
+
+// saída 
+System.Console.WriteLine(resultado);
---
+++ aula05/Exercicio02/Program.cs
@@ -0,0 +1,40 @@
+﻿
+string entrada;
+int maxX, maxY;
+int x, y;
+bool xValido, yValido;
+
+System.Console.WriteLine("Digite o valor máximo em X");
+entrada = Console.ReadLine();
+maxX = Convert.ToInt32(entrada);
+System.Console.WriteLine("Digite o valor máximo em Y");
+entrada = Console.ReadLine();
+maxY = Convert.ToInt32(entrada);
+
+System.Console.WriteLine("Qual a coordenada X?");
+entrada = Console.ReadLine();
+x = Convert.ToInt32(entrada);
+System.Console.WriteLine("Qual a coordenada Y?");
+entrada = Console.ReadLine();
+y = Convert.ToInt32(entrada);
+
+xValido = (x >= 0) && (x <= maxX);
+yValido = (y >= 0) && (y <= maxY);
+
+if( xValido && yValido )
+{
+    System.Console.WriteLine("Coordenada válida");
+}
+else 
+{
+    if(!xValido)
+    {
+        System.Console.WriteLine("Coordenada X inválida");
+    }
+
+    if(!yValido)
+    {
+        System.Console.WriteLine("Coordenada Y inválida");
+    }
+    
+}
