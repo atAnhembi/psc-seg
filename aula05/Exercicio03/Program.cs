02f87919f325b3cd50ccee576b90b88413070a2e
EmersonPaduan <emerson.paduan@gmail.com>
2024-09-09 11:48:38 -0300

    aula05 final

---
+++ aula05/Exercicio03/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int secreto, chute;
+
+Random geradorAleatorio  = new (); // criando um gerador de números "aleatórios"
+
+secreto = geradorAleatorio.Next(1, 11); // geara um número entre 1 e 10
+chute = 0;
+
+while (chute != secreto)
+{
+    System.Console.WriteLine("tente acertar o número secreto:");
+    chute = Convert.ToInt32(Console.ReadLine());
+
+    if (chute == secreto)
+    {
+        System.Console.WriteLine("Acertou! Parabéns!");
+    }
+    else
+    {
+        if (chute < secreto)
+        {
+            System.Console.WriteLine("O número secreto é maior");
+        }
+        else
+        {
+            System.Console.WriteLine("O número secreto é menor");
+        }
+    }
+}
---
+++ aula05/Exercicio04/Program.cs
@@ -0,0 +1,26 @@
+﻿float massaInicial, massaFinal;
+int numeroDeRepeticoes;
+
+System.Console.WriteLine("Digite a massa inicial");
+massaInicial = Convert.ToSingle(Console.ReadLine());
+
+massaFinal = massaInicial;
+
+numeroDeRepeticoes = 0;
+while( massaFinal >= 0.5 ) // enquanto a massa for maior ou igual
+{
+    massaFinal = massaFinal / 2;
+    // System.Console.WriteLine(massaFinal);
+    numeroDeRepeticoes++;
+}
+
+int segundos = numeroDeRepeticoes * 50;
+int minutos = segundos / 60;
+segundos = segundos % 60; // resto da divisão por 60 (quanto sobra depois que dividi por 60)
+
+int horas = minutos / 60;
+minutos = minutos % 60;
+
+System.Console.WriteLine($"Massa inicial: {massaInicial}g");
+System.Console.WriteLine($"Massa final: {massaFinal}g");
+System.Console.WriteLine($"Tempo: {horas}:{minutos}:{segundos}");
